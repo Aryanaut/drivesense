5b1bce557580ee8bc03a5dad15a455cc89b03e9a
Aryanaut <aryan.mahesh2010@gmail.com>
2019-07-01 07:01:56 +0530

    .

--- opencv-test.py
+++ opencv-test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
 
 cap = cv2.VideoCapture(0)
 
